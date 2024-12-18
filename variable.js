e60d7ad1f588e6b55a5747fcafc7cb0b7ddcac7e
adityajain256 <166713822+adityajain256@users.noreply.github.com>
2024-12-18 12:04:39 +0000

    day 1

---
+++ variable.js
@@ -0,0 +1,17 @@
+// variable,constant
+
+const accountId = 5451
+var accountName = "Aditya jain "
+let accountEmail = "jainaditya25522@gmail.com"
+accountCity = "Lalitpur"
+var accountPass = "14525"
+
+// account can not be changed it's in constant variable
+// accountName = "tanishka jain"
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountCity);
+
+console.table([accountEmail,accountId, accountName, accountCity]);
+
