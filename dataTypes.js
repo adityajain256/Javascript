da6cf4e2fb2f3dc7ff87e448e8df37f3de704652
adityajain256 <166713822+adityajain256@users.noreply.github.com>
2024-12-19 12:33:52 +0000

    day 2

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict"; // treat all JS code as newer version
+
+ //alert("hello"); we are using node.js not browser
+
+ console.log(5+5)
+ 
+ console.log("hello")
+ 
+ let name = "adi"
+ let age = 18
+ let isLoggedIn = false
+
+ // number => 2 to the power 53
+ // null 
+
+ // objesct 
+
+ //undefind 
+ // symbol 
+
+ console.log(typeof age);
+
+ console.log(typeof null);
+
+ console.log(typeof undefined);
