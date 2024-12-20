394acf1ebe1438fff572144ba2d6cc58f137fff5
adityajain256 <166713822+adityajain256@users.noreply.github.com>
2024-12-20 15:27:24 +0000

    day3

---
+++ dataType-convirsions.js
@@ -0,0 +1,18 @@
+let score = "45hh"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+ 
+console.log(valueInNumber);
+
+// "33" = 33
+/* "45hh"= NaN
+   "true"= 1
+   */
+
+   // 1 = true; 0= false
+   
