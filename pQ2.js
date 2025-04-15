f7a578fe8bff45a8147abfac5c0229a0789dcdb9
adityajain256 <jainaditya25522@gmail.com>
2025-04-15 22:23:10 +0530

    15-04-2025

---
+++ pQ2.js
@@ -0,0 +1,10 @@
+// take input and check if it is multypile of 5 or not 
+
+input = prompt("Enter the number: ");
+
+if(input%5==0){
+    alert("it is the multiple of 5")
+} else{
+    alert("it is not the multiple of 5")
+}
+
