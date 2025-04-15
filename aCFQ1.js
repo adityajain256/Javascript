9ecee41830c319804e1c467c021ed5eca95da6fe
adityajain256 <jainaditya25522@gmail.com>
2025-04-15 20:46:51 +0530

    15-04-2025

---
+++ aCFQ1.js
@@ -0,0 +1,12 @@
+const product =
+{
+    information: "Parker jotter standard CT Ball Pen (Black)",
+    rating: 7002,
+    offer: 5,
+    price: 270
+};
+
+console.log(product.information);
+console.log(product.rating);
+console.log(typeof product) ;
+console.log(product)
---
+++ ternaryOperater.js
@@ -0,0 +1,7 @@
+// basically it's a conditional operater works on condition ? true:flase i.e.
+
+let age = 15;
+
+let result = age>=18 ? "adult":"not-adult"
+
+console.log(result)
