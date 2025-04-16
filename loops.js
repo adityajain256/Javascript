c3fe11329081d79bbf7652623acd432120783cc7
adityajain256 <jainaditya25522@gmail.com>
2025-04-16 20:44:14 +0530

    16-04-2025

---
+++ loops.js
@@ -0,0 +1,13 @@
+// for loop for(let var=0; stopping; increament){}
+
+// for of loop
+
+str = "I am aditya jain";
+size = 0;
+
+for(let val of str){
+    console.log("iteration ", val);
+    size++;
+}
+
+console.log(size)
