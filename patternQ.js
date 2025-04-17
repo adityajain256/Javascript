f7be0f74cce75881942587d43da6a0d3281328c5
adityajain256 <jainaditya25522@gmail.com>
2025-04-17 17:09:48 +0530

    17

---
+++ patternQ.js
@@ -0,0 +1,32 @@
+// for normal star pattern 
+let n  = 5;
+
+// for(let i = 0; i<n; i++){
+//     for(let j = 0; j<=i; j++){
+//         console.log("*")
+//     }
+//     console.log("\n")
+// }
+
+// what if i made a reverse star pattern
+
+// for (let i=n-1; i>=0; i--){
+//     for(let j = i; j>=0; j--){
+//         console.log("*");
+        
+//     }
+//     console.log("\n");
+    
+    
+// }
+
+// what if i have to create a pattern in which star will start from right side 
+
+for(i=0;i<n;i++){
+    for(j=i; j>=0;j--){
+        console.log("8")
+
+    }
+    console.log("\n");
+    
+}
